8e546ab80493dda0515b489e3fe8f1f296e95da6
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-01-12 20:01:11 +0200

    class Main

---
+++ 1. Основы ООП/sampleTwo/Main.java
@@ -0,0 +1,16 @@
+package sampleTwo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Triangular triangular1 = new Triangular(5.5, 7.0, 7.5);
+		System.out.println(triangular1);
+		
+		Triangular triangular2 = new Triangular(5.1, 10.5, 8.3);
+		System.out.println(triangular2);
+		
+		Triangular triangular3 = new Triangular(5.3, 11.4, 10.5);
+		System.out.println(triangular3);
+	}
+}
