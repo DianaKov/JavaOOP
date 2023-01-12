5c2e500f945e8f5dd7ab9027203f3b8628f4a46b
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-01-12 19:58:31 +0200

    Create Main.java

---
+++ 1. Основы ООП/sample/Main.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Goods jacket1 = new Goods (5700, "black", "S");
+		Goods jacket2 = new Goods (5700, "white", "M");
+		Goods jacket3 = new Goods (6200, "pink", "S");
+		Goods jacket4 = new Goods (5800, "white", "S");
+		Goods jacket5 = new Goods (4200, "black", "S");
+		Goods jacket6 = new Goods (4200, "black", "M");
+
+		System.out.println(jacket1);
+		System.out.println(jacket2);
+		System.out.println(jacket3);
+		System.out.println(jacket4);
+		System.out.println(jacket5);
+		System.out.println(jacket6);
+	}
+}
